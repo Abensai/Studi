554556e255b23480ab1e31ae173b69d1330de54e
Abensai <ali.bensaiad@gmail.com>
2022-04-01 22:45:38 +0200

    Template Integration theme static

--- src/Controller/EstablishmentController.php
+++ src/Controller/EstablishmentController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class EstablishmentController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/establishment", name="app_establishment")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('establishment/index.html.twig', [
+            'controller_name' => 'EstablishmentController',
         ]);
     }
 }
--- src/Controller/SuiteController.php
+++ src/Controller/SuiteController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class SuiteController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/suite", name="app_suite")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('suite/index.html.twig', [
+            'controller_name' => 'SuiteController',
         ]);
     }
 }
