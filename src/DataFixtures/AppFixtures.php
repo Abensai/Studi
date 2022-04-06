948b64526e7927bc5ebdb4afb1cd4c1025f2b9a8
Abensai <ali.bensaiad@gmail.com>
2022-04-06 22:04:52 +0200

    Fixtures optim fixtures load + gallery

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Establishment;
+use App\Entity\Gallery;
 use App\Entity\Service;
 use App\Entity\Suite;
 use App\Entity\User;
@@ -21,10 +22,15 @@ class AppFixtures extends Fixture
         $this->passwordHashes = $passwordHashes;
     }
 
+    /**
+     * @param ObjectManager $manager
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
+        // Data
         $user = new stdClass();
         $user->userAll = [
             'admin@admin.com',
@@ -48,8 +54,9 @@ class AppFixtures extends Fixture
             'Plaza Hotel',
             'Ritz-Carlton Hotel',
         ];
-
         $counter = 0;
+
+        // User & Establishment Fixtures
         foreach ($user->userAll as $email) {
             $user = new User();
 
@@ -59,7 +66,6 @@ class AppFixtures extends Fixture
                 ->setTelephone($faker->phoneNumber());
 
             $user->setPassword(self::hashPassword($user));
-            $manager->persist($user);
 
             if (str_contains($email, 'manager')) {
                 $establishment = new Establishment();
@@ -71,48 +77,104 @@ class AppFixtures extends Fixture
                     ->setPageWeb($faker->url())
                     ->setUser($user)
                     ->setSlug($faker->slug(3));
-
-                $user->setEstablishment($establishment);
                 $manager->persist($establishment);
 
-                for ($i = 0; $i < 6; ++$i) {
-                    $room = new Suite();
+                $user->setEstablishment($establishment);
 
-                    $room->setTitre($faker->words(3, true))
-                        ->setDescription($faker->text(250))
-                        ->setSlug($faker->slug(3))
-                        ->setEstablishment($establishment)
-                        ->setDisponibilite(true)
-                        ->setLienBooking($faker->url())
-                        ->setPrix($faker->randomFloat(2, 500, 9999))
-                        ->setImage('gallery/lux-'.strval($i).'jpg');
+                $this->addReference('establishment_'.$counter, $establishment);
 
-                    $manager->persist($room);
+                ++$counter;
+            }
 
-                    for ($i = 0; $i < 6; ++$i) {
-                        $service = new Service();
+            $manager->persist($user);
+        }
 
-                        $service->setTitre($faker->words(3, true))
-                            ->setDescription($faker->text(200))
-                            ->setPrix($faker->randomFloat(2, 10, 999))
-                            ->setSlug($faker->slug(3))
-                            ->addSuite($room);
+        // Suite Fixtures
+        $key = 0;
+        for ($i = 0; $i < 3; ++$i) {
+            for ($j = 0; $j < 6; ++$j) {
+                $suite = new Suite();
 
-                        $room->addService($service);
+                $suite->setTitre($faker->words(3, true))
+                    ->setDescription($faker->text(250))
+                    ->setSlug($faker->slug(3))
+                    ->setEstablishment($this->getReference("establishment_$i"))
+                    ->setDisponibilite(true)
+                    ->setLienBooking($faker->url())
+                    ->setPrix($faker->randomFloat(2, 500, 9999))
+                    ->setImage('gallery/lux-'.strval($j).'jpg');
 
-                        $manager->persist($service);
-                    }
-                }
+                $this->addReference('suite_'.$key, $suite);
+
+                $manager->persist($suite);
+                ++$key;
+            }
+        }
+
+        // Service Fixtures
+        for ($i = 0; $i < 18; ++$i) {
+            for ($j = 0; $j < 4; ++$j) {
+                $service = new Service();
+
+                $suite = $this->getReference("suite_$i");
+
+                $service->setTitre($faker->words(3, true))
+                    ->setDescription($faker->text(200))
+                    ->setPrix($faker->randomFloat(2, 10, 999))
+                    ->setSlug($faker->slug(3))
+                    ->addSuite($suite);
+
+                $suite->addService($service);
+
+                $manager->persist($service);
+            }
+        }
+
+        // Gallery Fixtures
+        $galleryCollection = [];
+        foreach (glob('./assets/gallery/*') as $filename) {
+            $galleryCollection[] = $filename;
+        }
+
+        $counterImg = 0;
+        for ($i = 0; $i < 18; ++$i) {
+            for ($j = 0; $j < 3; ++$j) {
+                $gallery = new Gallery();
+
+                $gallery->setTitre(self::getImageName($galleryCollection[$counterImg]))
+                    ->setLien($galleryCollection[$counterImg])
+                    ->setTaille(filesize($galleryCollection[$counterImg]))
+                    ->setSlug($faker->slug())
+                    ->setSuite($this->getReference("suite_$i"));
+                $manager->persist($gallery);
+                ++$counterImg;
             }
         }
 
         $manager->flush();
     }
 
+    /**
+     * @param User $user
+     * @return string
+     */
     private function hashPassword(User $user): string
     {
         return $this->passwordHashes->hashPassword(
             $user, 'password'
         );
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return mixed|string
+     */
+    private function getImageName(string $url)
+    {
+        $name = explode('/', $url);
+        $name = explode('.', $name[3]);
+
+        return $name[0];
     }
 }
