64a9aa7f7a334a7d1bde8850f97b8ee3213a4ecc
Abensai <ali.bensaiad@gmail.com>
2022-04-16 18:50:38 +0200

    first test fix and readme file

--- migrations/Version20220416131438.php
+++ migrations/Version20220416131438.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220413181108 extends AbstractMigration
+final class Version20220416131438 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -25,55 +25,55 @@ final class Version20220413181108 extends AbstractMigration
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, suite_id INT NOT NULL, establishment_id INT NOT NULL, date_debut DATETIME NOT NULL, date_fin DATETIME NOT NULL, prix NUMERIC(7, 2) NOT NULL, INDEX IDX_E00CEDDEA76ED395 (user_id), UNIQUE INDEX UNIQ_E00CEDDE4FFCB518 (suite_id), INDEX IDX_E00CEDDE8565851 (establishment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, suite_id INT NOT NULL, establishment_id INT NOT NULL, date_debut DATETIME NOT NULL, date_fin DATETIME NOT NULL, prix NUMERIC(7, 2) NOT NULL, INDEX IDX_E00CEDDEA76ED395 (user_id), UNIQUE INDEX UNIQ_E00CEDDE4FFCB518 (suite_id), INDEX IDX_E00CEDDE8565851 (establishment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prenom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, sujet VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, message LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, date DATETIME NOT NULL, statut TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prenom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, sujet VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, message LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, date DATETIME NOT NULL, statut TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE establishment (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, nom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ville VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, adresse VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, page_web VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, image VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_DBEFB1EEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE establishment (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, nom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ville VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, adresse VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, page_web VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, image VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, UNIQUE INDEX UNIQ_DBEFB1EEA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE gallery (id INT AUTO_INCREMENT NOT NULL, suite_id INT NOT NULL, titre VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, lien VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_472B783A4FFCB518 (suite_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE gallery (id INT AUTO_INCREMENT NOT NULL, suite_id INT NOT NULL, titre VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, lien VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, INDEX IDX_472B783A4FFCB518 (suite_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, headers LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, queue_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, headers LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, queue_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prix NUMERIC(6, 2) NOT NULL, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, icon VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prix NUMERIC(6, 2) NOT NULL, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, icon VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE service_suite (service_id INT NOT NULL, suite_id INT NOT NULL, INDEX IDX_BC4E09C1ED5CA9E6 (service_id), INDEX IDX_BC4E09C14FFCB518 (suite_id), PRIMARY KEY(service_id, suite_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE service_suite (service_id INT NOT NULL, suite_id INT NOT NULL, INDEX IDX_BC4E09C1ED5CA9E6 (service_id), INDEX IDX_BC4E09C14FFCB518 (suite_id), PRIMARY KEY(service_id, suite_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE suite (id INT AUTO_INCREMENT NOT NULL, establishment_id INT NOT NULL, titre VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, image VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prix NUMERIC(7, 2) NOT NULL, lien_booking VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, disponibilite TINYINT(1) NOT NULL, INDEX IDX_153CE4268565851 (establishment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE suite (id INT AUTO_INCREMENT NOT NULL, establishment_id INT NOT NULL, titre VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, image VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, description LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prix NUMERIC(7, 2) NOT NULL, lien_booking VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, disponibilite TINYINT(1) NOT NULL, INDEX IDX_153CE4268565851 (establishment_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->abortIf(
             !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQL57Platform,
             "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQL57Platform'."
         );
 
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prenom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, nom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, telephone VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON NOT NULL, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prenom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, nom VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, telephone VARCHAR(50) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 
     public function down(Schema $schema): void
--- readme.md
+++ readme.md
@@ -3,41 +3,83 @@ Studi ECF
 
 Application Symfony permettant de réserver des suites du groupe hôtelier Hypnos
 
-## Installation en local
+## Pré-requis
+* PHP 7.4
+* Composer 2.2.9
+* Symfony CLI 5.4.1
+* nodejs 16.14.0
+* npm 8.5.5
 
-### Pré-requis
-* PHP 8.1
-* Composer
-* Symfony CLI
-* Docker-compose
-* nodejs et npm
-
-Les pré-requis peuvent être vérifié avec la commande suivante {de la CLI Symfony}: {sauf pour la cas de Docker et Docker-compose}
+Les pré-requis peuvent être vérifié avec la commande suivante {de la CLI Symfony}:
 ```bash
 symfony check:requirements
 ```
-## Installation en local
 
-```bash
+### Installation en local
 
+  - Dans votre dossier local faire un git clone:
+```python
+git clone https://github.com/Abensai/Studi.git
+```
+  - rejoindre la racine du projet
+```python
+cd ./Studi
+```
+   - Installer les bundles et toutes les dependances avec composer
+```python
+composer install
+```
+   - créer un fichier à la racine .env.local et y copier le contenu présent dans le fichier dèjà présent .env
+   - dans votre fichier .env.local, modifier la ligne suivante avec vos identifiants de base de données local (l. 30)
+```python
+DATABASE_URL="mysql://db_user:db_password@127.0.0.1:3306/db_name?serverVersion=5.7"
+```
+  - renseigner les informations suivantes:
+        - db_user
+        - db_password
+        - num du server par défault à 3306
+        - db_name
+
+  - créer une base de données avec la commande suivante: par default db_name = database
+```python
+symfony console doctrine:database:create {db_name}
+```  
+   - importer les tables vides dans cette nouvelle bdd (database) avec la commande suivante: confirmer avec yes
+```python
+symfony console doctrine:migrations:migrate
+``` 
+  - importer les données dans les tables vides avec la commande suivante: confirmer avec yes   
+```python
+symfony console doctrine:fixtures:load 
+``` 
+   - installer les dependances avec npm
+```python
+npm install
+```
+   - créer les fichiers public avec la commande suivante:
+```python
+npm run build
+```
+   - lancer la commande symfony server
+```python
+symfony server:start -d
 ```
 
-## Lancer l'environnement de développement
+### Lancer l'environnement de développement
 
 ```bash
 composer install
 npm install
 npm run build
-symfony serve:start -d
-
+symfony server:start -d
 ```
 
-## Ajouter des données de tests
+### Ajouter des données de tests
 ```bash
 symfony console doctrine:fixtures:load
 ```
 
-## Lancer tests en local
+### Lancer tests en local
 ```bash
 php bin/phpunit --testdox
 ```
@@ -46,19 +88,28 @@ php bin/phpunit --testdox
 
 ```python
 
-
-# Commandes
-
-
 # Front
-
+login admin:
+    email: admin@admin.com
+    password: password
+login manager:
+    email: manager-one@manager.com
+    password: password
 
 # Back-Office
+le fichier des fixtures créé un user admin
+email: admin@admin.com
+lien: /admin
+roles permissions: ROLE_ADMIN, ROLE_MANAGER
 
+la modification des users, des etablissement, et des roles par 
+l'administrateur va pouvoir se faire depuis le back-office
+
+le manager va pouvoir modifier les suites, après que l'admin lui ai
+ajouté le role ROLE_MANAGER depuis de back-office 
 ```
-## Amélioration
 
-### Envoie des mails de Contacts
+## Envoie des mails de Contacts
 
 Les mails de prise de contact sont stockés en base de données, pour les envoyer à l'admin par mail 
 dans un second temps, il faudrait mettre en place un cron sur :
