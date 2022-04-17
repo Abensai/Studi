2f0a41035634063fc9e7897e1fc358ba2644d3b0
Abensai <ali.bensaiad@gmail.com>
2022-04-17 20:26:50 +0200

    test and fix

--- src/Controller/DashboardUserController.php
+++ src/Controller/DashboardUserController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class InfosUserController extends AbstractController
+class DashboardUserController extends AbstractController
 {
     /**
      * @Route("/infos", name="app_infos")
